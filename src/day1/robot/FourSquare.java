5babdd5e8baa19c4f4c179f2c27c065659b4adc1
zwallace2021 <league@WTS19.attlocal.net>
2017-08-01 11:53:08 -0700

    Day 2
    Introduction to Javascript

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -1,3 +1,5 @@
+
+
 package day1.robot;
 
 import javax.swing.JOptionPane;
@@ -6,19 +8,28 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-	
+	Robot r2d2 = new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
+r2d2.setSpeed(10);
 
 		// 5. Set the pen width to 5
+r2d2.setPenWidth(5);
+r2d2.penDown();
 
 		// 6. Do steps #7 to #8 four times...
+for (int i = 0; i < 4; i++) {
+	r2d2.setRandomPenColor();
+	
 
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
-	
+
+		r2d2.move(200);
+		r2d2.turn(90);
+	}
 			// 8. Turn the robot 90 degrees to the right
 
 	}
